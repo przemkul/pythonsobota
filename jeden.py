025e045413064cdd5b54ce22079a678c09650bee
przemkul <przemkul@gmail.com>
2019-10-26 15:48:57 +0200

    pierwsza wersja

---
+++ jeden.py
@@ -0,0 +1,15 @@
+#tworzymy własną funkce
+#def funkcja_moja():
+    #print("hej")
+
+#wywołanie funkcji 
+#funkcja_moja()
+
+a = 'witam'
+b = ' w swiecie '
+c = 'pythona'
+
+def wypisz():
+    print(a,b,c) 
+
+wypisz()
